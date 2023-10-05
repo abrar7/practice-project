6d961472923192a7491025aae49a5a32576c5ef8
Abrar-ul-Haq <abrarmughal003@gmail.com>
2023-10-06 00:55:21 +0500

    issue-fixed

--- components/AppNavigator.js
+++ components/AppNavigator.js
@@ -24,24 +24,24 @@ const Stack = createNativeStackNavigator();
 // ===================================================================
 
 export default function AppNavigator() {
-  const [userRole, setUserRole] = useState();
+  // const [userRole, setUserRole] = useState();
   const [initializing, setInitializing] = useState(true);
   const [user, setUser] = useState(null);
   const database = FIRESTORE_DB;
 
-  async function getData() {
-    const response = await getDocs(
-      query(
-        collection(database, "user"),
-        where("id", "==", FIREBASE_AUTH?.currentUser?.uid)
-      )
-    );
-    const documentData = response.docs.map((document) => ({
-      id: document.id,
-      ...document.data(),
-    }));
-    setUserRole(documentData[0].role);
-  }
+  // async function getData() {
+  //   const response = await getDocs(
+  //     query(
+  //       collection(database, "user"),
+  //       where("id", "==", FIREBASE_AUTH?.currentUser?.uid)
+  //     )
+  //   );
+  //   const documentData = response.docs.map((document) => ({
+  //     id: document.id,
+  //     ...document.data(),
+  //   }));
+  //   setUserRole(documentData[0].role);
+  // }
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(FIREBASE_AUTH, (authUser) => {
@@ -50,8 +50,8 @@ export default function AppNavigator() {
         setInitializing(false);
       }
     });
-    getData();
-    console.log("userRole", userRole);
+    // getData();
+    // console.log("userRole", userRole);
 
     return unsubscribe;
   }, []);
@@ -60,51 +60,85 @@ export default function AppNavigator() {
     return <AppLoader />;
   }
 
-  // console.log("user===", user);
+  // console.log("user=============", user);
+  const userRole = "admin";
+  // const userRole = "customer";
 
-  return (
+  return user ? (
     <Stack.Navigator
-      initialRouteName={user ? "customerHomeScreen" : "confirmUser"}
+      initialRouteName={
+        userRole === "customer" ? "customerHomeScreen" : "adminHomeScreen"
+      }
     >
       <Stack.Screen name="adminHomeScreen" component={AdminHomeScreen} />
       <Stack.Screen name="addItemForm" component={AddItemForm} />
+      <Stack.Screen name="customerHomeScreen" component={CustomerHomeScreen} />
+      <Stack.Screen name="scannerComponent" component={ScannerComponent} />
+      <Stack.Screen name="generateQRCode" component={GenerateQRCode} />
+      <Stack.Screen name="checkoutPage" component={CheckoutPage} />
+      <Stack.Screen name="itemCards" component={ItemCards} />
+    </Stack.Navigator>
+  ) : (
+    <Stack.Navigator initialRouteName="confirmUser">
       <Stack.Screen name="confirmUser" component={ConfirmUser} />
       <Stack.Screen name="customerAction" component={CustomerAction} />
       <Stack.Screen name="signUp" component={SignupView} />
       <Stack.Screen name="login" component={LoginView} />
       <Stack.Screen name="forgotPassword" component={ForgotPasswordView} />
-      <Stack.Screen name="customerHomeScreen" component={CustomerHomeScreen} />
-      <Stack.Screen name="scannerComponent" component={ScannerComponent} />
-      <Stack.Screen name="generateQRCode" component={GenerateQRCode} />
-      <Stack.Screen name="checkoutPage" component={CheckoutPage} />
-      <Stack.Screen
-        name="itemCards"
-        component={ItemCards}
-        options={{ headerShown: false }}
-      />
     </Stack.Navigator>
   );
-  // return user ? (
-  //   // <Stack.Navigator initialRouteName="customerHomeScreen">
-  //   <Stack.Navigator initialRouteName="customerHomeScreen">
-  //     <Stack.Screen name="customerHomeScreen" component={CustomerHomeScreen} />
-  //     <Stack.Screen name="scannerComponent" component={ScannerComponent} />
-  //     <Stack.Screen name="generateQRCode" component={GenerateQRCode} />
-  //     <Stack.Screen
-  //       name="itemCards"
-  //       component={ItemCards}
-  //       options={{ headerShown: false }}
-  //     />
-  //   </Stack.Navigator>
-  // ) : (
-  //   <Stack.Navigator initialRouteName="adminHomeScreen">
-  //     <Stack.Screen name="adminHomeScreen" component={AdminHomeScreen} />
-  //     <Stack.Screen name="addItemForm" component={AddItemForm} />
-  //     <Stack.Screen name="confirmUser" component={ConfirmUser} />
-  //     <Stack.Screen name="customerAction" component={CustomerAction} />
-  //     <Stack.Screen name="signUp" component={SignupView} />
-  //     <Stack.Screen name="login" component={LoginView} />
-  //     <Stack.Screen name="forgotPassword" component={ForgotPasswordView} />
-  //   </Stack.Navigator>
-  // );
 }
+
+// {
+//   /* <Stack.Navigator initialRouteName="customerHomeScreen">
+//   <Stack.Screen name="addItemForm" component={AddItemForm} />
+//   <Stack.Screen name="adminHomeScreen" component={AdminHomeScreen} />
+//   <Stack.Screen name="customerHomeScreen" component={CustomerHomeScreen} />
+//   <Stack.Screen name="scannerComponent" component={ScannerComponent} />
+//   <Stack.Screen name="generateQRCode" component={GenerateQRCode} />
+//   <Stack.Screen name="checkoutPage" component={CheckoutPage} />
+//   <Stack.Screen name="itemCards" component={ItemCards} />
+// </Stack.Navigator>; */
+// }
+
+// user ? (
+//   <Stack.Navigator
+//     initialRouteName={
+//       userRole === "customer" ? "customerHomeScreen" : "adminHomeScreen"
+//     }
+//   >
+//     <Stack.Screen name="adminHomeScreen" component={AdminHomeScreen} />
+//     <Stack.Screen name="addItemForm" component={AddItemForm} />
+//     <Stack.Screen name="customerHomeScreen" component={CustomerHomeScreen} />
+//     <Stack.Screen name="scannerComponent" component={ScannerComponent} />
+//     <Stack.Screen name="generateQRCode" component={GenerateQRCode} />
+//     <Stack.Screen name="checkoutPage" component={CheckoutPage} />
+//     <Stack.Screen name="itemCards" component={ItemCards} />
+//   </Stack.Navigator>
+// ) : (
+//   <Stack.Navigator initialRouteName="confirmUser">
+//     <Stack.Screen name="confirmUser" component={ConfirmUser} />
+//     <Stack.Screen name="customerAction" component={CustomerAction} />
+//     <Stack.Screen name="signUp" component={SignupView} />
+//     <Stack.Screen name="login" component={LoginView} />
+//     <Stack.Screen name="forgotPassword" component={ForgotPasswordView} />
+//   </Stack.Navigator>
+// );
+
+// userRole === "customer" ? (
+//   <Stack.Navigator initialRouteName="customerHomeScreen">
+//     <Stack.Screen
+//       name="customerHomeScreen"
+//       component={CustomerHomeScreen}
+//     />
+//     <Stack.Screen name="scannerComponent" component={ScannerComponent} />
+//     <Stack.Screen name="generateQRCode" component={GenerateQRCode} />
+//     <Stack.Screen name="checkoutPage" component={CheckoutPage} />
+//     <Stack.Screen name="itemCards" component={ItemCards} />
+//   </Stack.Navigator>
+// ) : (
+//   <Stack.Navigator initialRouteName="adminHomeScreen">
+//     <Stack.Screen name="adminHomeScreen" component={AdminHomeScreen} />
+//     <Stack.Screen name="addItemForm" component={AddItemForm} />
+//   </Stack.Navigator>
+// )
